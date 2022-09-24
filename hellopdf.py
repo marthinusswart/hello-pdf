0925bad5ca788408ae2ebf2942e371568104a37c
marthinusswart <matt.swart@icloud.com>
2022-09-24 17:07:44 +1000

    first commit

---
+++ hellopdf.py
@@ -0,0 +1,9 @@
+# Import the required Module
+import tabula
+# Read a PDF File
+#df = tabula.read_pdf("/home/matt/anz-stmt.pdf", pages='all')
+# convert PDF into CSV
+#df.to_csv('/home/matt/anz-stmt.csv', encoding='utf-8')
+#print(df)
+# convert PDF into CSV file
+tabula.convert_into("/home/matt/anz-stmt.pdf", "/home/matt/anz-stmt.csv", output_format="csv", pages='all')
