3c2c298aefcf53640c088255e411ab155c041d4e
marthinusswart <matt.swart@icloud.com>
2022-09-24 17:09:31 +1000

    latest

--- hellopdf.py
+++ hellopdf.py
@@ -4,6 +4,8 @@ import tabula
 #df = tabula.read_pdf("/home/matt/anz-stmt.pdf", pages='all')
 # convert PDF into CSV
 #df.to_csv('/home/matt/anz-stmt.csv', encoding='utf-8')
-#print(df)
+# print(df)
 # convert PDF into CSV file
-tabula.convert_into("/home/matt/anz-stmt.pdf", "/home/matt/anz-stmt.csv", output_format="csv", pages='all')
+tabula.convert_into("/home/matt/anz-stmt.pdf",
+                    "/home/matt/anz-stmt.csv", output_format="csv", pages='all')
+print('done')
